a0372c8bb0b09936b8b51bcde488dc110ccb006f
vladandrei <vlad.andrei@flip.ro>
2022-07-20 19:21:04 +0300

    small refactor

--- src/models/friends.schema.ts
+++ src/models/friends.schema.ts
@@ -1,16 +1,21 @@
 import { Schema, model } from "mongoose";
-import { IUser } from "./user.schema";
+import { v4 as uuidv4 } from "uuid";
+
+import { IUser } from "models/user.schema";
 
 export interface Friend {
-  id: string;
+  _id: string;
   requester: IUser,
   recipient: IUser,
   status: number
 }
 
-
 const friendsSchema = new Schema<Friend>(
   {
+    _id: {
+      type: String,
+      default: () => uuidv4().replaceAll("-", ""),
+    },
     requester: { type: Schema.Types.ObjectId, ref: 'Users'},
     recipient: { type: Schema.Types.ObjectId, ref: 'Users'},
     status: {
